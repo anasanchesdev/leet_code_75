33a95e50a275f60a959a9c1bee80021722e6ee43
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-05-29 11:46:43 -0300

    RUNTIME: 39ms (beats 40.36%)  MEMORY: 16.69MB (beats 79.91%)

--- 07_ProductofArrayExcept Self/mysolution.py
+++ 07_ProductofArrayExcept Self/mysolution.py
@@ -1,13 +1,17 @@
 class Solution:
-    def productExceptSelf(self, nums: list[int]) -> list[int]:
+    @staticmethod
+    def productExceptSelf(nums: list[int]) -> list[int]:
         answer = [number for number in nums]
         mult = 1
 
         for index1, num1 in enumerate(nums):
+
             for index2, num2 in enumerate(nums):
+                print(f'index 1: {index1} index 2: {index2}')
                 if index1 == index2:
                     continue
                 mult *= num2
+
             answer[index1] = mult
             mult = 1
 
