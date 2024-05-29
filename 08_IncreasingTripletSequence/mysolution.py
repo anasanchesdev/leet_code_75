f5dc9e04f073b251ecbdcba6550b27ac9b176e21
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-05-29 11:47:03 -0300

    Initial commit

---
+++ 08_IncreasingTripletSequence/mysolution.py
@@ -0,0 +1,9 @@
+nums = [20, 100, 10, 12, 5, 13]
+# for index in range(len(nums) - 2):
+#     if nums[index] < nums[index + 1] < nums [index + 2]:
+#         print(True)
+
+nums.sort()
+print(nums)
+for index in range(len(nums) - 2):
+    ...
